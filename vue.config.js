b4e353aee2aef172397204b6fde5cf3a82584c05
Mike28E <Michael94E@hotmail.com>
2023-09-25 22:19:17 -0700

    updates

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ],
-  publicPath: "/"
-  // publicPath: process.env.NODE_ENV === "production" ? "/car-rental-site/" : "/",
+  ]
 })
