8d06a55f4f50046c8d45fe1e2033bf7abd764fb9
Mike28E <Michael94E@hotmail.com>
2024-03-20 19:20:26 -0700

    crisp chat

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueGtag from "vue-gtag";
 import ScrollAnimation from './directives/scrollanimation'
+import { Crisp } from "crisp-sdk-web";
 
 Vue.directive('scrollanimation', ScrollAnimation);
 
@@ -11,6 +12,10 @@ Vue.config.productionTip = false
 Vue.use(VueGtag, {
   config: { id: "G-Y1WEC995D7" }
 });
+
+Crisp.configure("659c5cc9-c68c-4ce8-820f-0e225274771e")
+
+Vue.use(Crisp)
 
 new Vue({
   vuetify,
